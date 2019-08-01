0f0f4bae7baec9293a45cba62d18f3e3554554d5
konglingyinxia <hedankly@foxmail.com>
2019-08-01 10:02:15 +0800

    增加公共状态

--- project-platform-item/src/main/java/com/kongling/platform/controller/common/CommonStatusController.java
+++ project-platform-item/src/main/java/com/kongling/platform/controller/common/CommonStatusController.java
@@ -1,9 +1,19 @@
 package com.kongling.platform.controller.common;
 
+import com.kongling.platform.entity.VO.StatusVo;
+import com.kongling.platform.entity.enums.rolepermission.PermissionsMenuTypeEnum;
+import config.Respons.ResponseUtil;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Scope;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 公共状态值
@@ -18,7 +28,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CommonStatusController {
 
 
-
+    /**
+     * 菜单类型code
+     */
+    @RequestMapping(value = "getMenuType", method = RequestMethod.GET)
+    @ResponseBody
+    @ApiOperation(value = "菜单类型code")
+    public Map<String, Object> getMenuType() {
+        List<StatusVo> list = new ArrayList<>();
+        for (PermissionsMenuTypeEnum o : PermissionsMenuTypeEnum.values()) {
+            StatusVo vo = new StatusVo();
+            vo.setCode(o.getCode().intValue());
+            vo.setName(o.getMessage());
+            list.add(vo);
+        }
+        return ResponseUtil.getSuccessMap(list);
+    }
 
 
 
